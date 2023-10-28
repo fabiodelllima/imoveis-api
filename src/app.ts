4322277796010f87c6c8f054a1cea34eae5f22dc
fabiodelllima <f.f.fabiodelima@gmail.com>
2023-10-28 16:40:54 -0300

    feat: Index das rotas

--- src/app.ts
+++ src/app.ts
@@ -3,10 +3,12 @@ import 'reflect-metadata';
 import 'express-async-errors';
 import express, { Application, json } from 'express';
 import { handleErrors } from './middlewares/handleErrors.middleware';
+import { routes } from './routers/index.route';
 
 const app: Application = express();
 
 app.use(json());
+app.use('/', routes);
 app.use(handleErrors);
 
 export default app;
---
+++ src/routers/index.route.ts
@@ -0,0 +1,5 @@
+import { Router } from 'express';
+import { userRouter } from './users.route';
+
+export const routes: Router = Router();
+
