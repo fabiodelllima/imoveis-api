96556d05fb584922b3a4376b71cf70b0a5d3ddb8
fabiodelllima <f.f.fabiodelima@gmail.com>
2023-10-28 05:12:36 -0300

    feat: Middleware handleErrors

--- src/app.ts
+++ src/app.ts
@@ -2,9 +2,11 @@ import 'reflect-metadata';
 import 'reflect-metadata';
 import 'express-async-errors';
 import express, { Application, json } from 'express';
+import { handleErrors } from './middlewares/handleErrors.middleware';
 
 const app: Application = express();
 
 app.use(json());
+app.use(handleErrors);
 
 export default app;
