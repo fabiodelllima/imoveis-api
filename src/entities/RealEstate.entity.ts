9aa14eea1404ca49129682763a56b7b15f19b7b3
fabiodelllima <f.f.fabiodelima@gmail.com>
2023-10-28 03:19:30 -0300

    feat: Entidade RealEstate

---
+++ src/entities/RealEstate.entity.ts
@@ -0,0 +1,48 @@
+import {
+  Column,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  OneToMany,
+  OneToOne,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+import Schedule from './Schedule.entity';
+import Address from './Address.entity';
+import Category from './Category.entity';
+
+@Entity('realEstates')
+export default class RealEstate {
+  @PrimaryGeneratedColumn('increment')
+  id: number;
+
+  @Column({ default: 0 })
+  sold: boolean;
+
+  @Column({
+    type: 'decimal',
+    precision: 12,
+    scale: 2,
+    default: 0,
+  })
+  value: number;
+
+  @Column()
+  size: number;
+
+  @Column({ type: 'date' })
+  createdtAt: string;
+
+  @Column({ type: 'date' })
+  updatedAt: string;
+
+  @OneToMany(() => Schedule, (schedule) => schedule.realEstate)
+  schedules: Schedule[];
+
+  @OneToOne(() => Address, (address) => address.realEstate)
+  @JoinColumn()
+  address: Address;
+
+  @ManyToOne(() => Category, (category) => category.realEstates)
+  category: Category;
+}
