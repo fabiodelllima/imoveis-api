535215857e0186f25d8228cab7c387c02dee7876
fabiodelllima <f.f.fabiodelima@gmail.com>
2023-10-31 20:52:19 -0300

    chore: Linha comentada removida

--- src/interfaces/schedules.interface.ts
+++ src/interfaces/schedules.interface.ts
@@ -2,5 +2,3 @@ import { z } from 'zod';
 import { createNewSchedule } from '../schemas/schedules.schema';
 
 export type TCreateSchedule = z.infer<typeof createNewSchedule>;
-
-// export type TScheduleRepo = Repository<Schedule>;
