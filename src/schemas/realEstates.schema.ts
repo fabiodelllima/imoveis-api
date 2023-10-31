75074b5b5ae7e7a751727e99102a8b2627d0de15
fabiodelllima <f.f.fabiodelima@gmail.com>
2023-10-30 21:56:18 -0300

    feat: Schema de RealEstates

---
+++ src/schemas/realEstates.schema.ts
@@ -0,0 +1,18 @@
+import { z } from 'zod';
+
+export const realEstateSchema = z.object({
+  id: z.number().positive(),
+  sold: z.boolean().default(false),
+  value: z.number(),
+  size: z.number(),
+  createdtAt: z.string(),
+  updatedAt: z.string(),
+  address: z.object({
+    street: z.string().max(45),
+    zipCode: z.string().max(8),
+    number: z.number(),
+    city: z.string().max(20),
+    state: z.string().max(2),
+  }),
+  categoryName: z.string().max(45),
+});
