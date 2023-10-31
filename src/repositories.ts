18cf64f2177cb205b05451a56a987e84545a1bd4
fabiodelllima <f.f.fabiodelima@gmail.com>
2023-10-31 04:01:55 -0300

    feat: Instância de Repository para RealEstates

--- src/repositories.ts
+++ src/repositories.ts
@@ -1,8 +1,13 @@
 import { Repository } from 'typeorm';
 import Schedule from './entities/Schedule.entity';
 import { AppDataSource } from './data-source';
+import RealEstate from './entities/RealEstate.entity';
 
 export const scheduleRepo: Repository<Schedule> =
   AppDataSource.getRepository(Schedule);
 
-// export const scheduleRepo: TScheduleRepo = AppDataSource.getRepository(Schedule);
+export const realEstateRepo: Repository<RealEstate> =
+  AppDataSource.getRepository(RealEstate);
+
+
+// [?] export const scheduleRepo: TScheduleRepo = AppDataSource.getRepository(Schedule); [?] 
