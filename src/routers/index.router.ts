44fe386ec4cc5f21dfb3b57b9ebb9be14cd51ea4
fabiodelllima <f.f.fabiodelima@gmail.com>
2023-11-01 15:33:54 -0300

    feat: Rota de Login

--- src/routers/index.router.ts
+++ src/routers/index.router.ts
@@ -1,6 +1,8 @@
 import { Router } from 'express';
 import { userRouter } from './users.router';
+import { loginRouter } from './session.router';
 
 export const routes: Router = Router();
 
 routes.use('/users', userRouter);
+routes.use('/login', loginRouter);
