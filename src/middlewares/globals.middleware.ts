31be339d23544094bafe337f35a3a91cf00c1290
fabiodelllima <f.f.fabiodelima@gmail.com>
2023-11-01 15:05:29 -0300

    refactor: Reorganização em middlewares globais

--- src/middlewares/globals.middleware.ts
+++ src/middlewares/globals.middleware.ts
@@ -1,11 +1,15 @@
+import AppError from '../errors/AppError.error';
 import { NextFunction, Request, Response } from 'express';
 import { ZodTypeAny } from 'zod';
+import { verify } from 'jsonwebtoken';
 
 export const validateBody =
   (schema: ZodTypeAny) =>
   (req: Request, res: Response, next: NextFunction): void => {
     req.body = schema.parse(req.body);
 
+    return next();
+  };
 
 export const verifyToken = (
   req: Request,
