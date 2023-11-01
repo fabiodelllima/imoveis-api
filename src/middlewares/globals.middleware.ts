9a4de5c6fd6bb2476be11dd125fb26177a2f941e
fabiodelllima <f.f.fabiodelima@gmail.com>
2023-11-01 15:04:04 -0300

    feat: Middleware verifyPermissions

--- src/middlewares/globals.middleware.ts
+++ src/middlewares/globals.middleware.ts
@@ -37,3 +37,18 @@ export const verifyAdmin = (
   return next();
 };
 
+export const verifyPermissions = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+): void => {
+  const { id } = req.params;
+  const { sub, admin } = res.locals.decoded;
+
+  if (admin) return next();
+
+  if (id !== sub)
+    throw new AppError('Insufficient permissions', 409);
+
+  return next();
+};
