3da6b1818c1843ec28f50785a285d7d247eadfeb
fabiodelllima <f.f.fabiodelima@gmail.com>
2023-11-01 15:03:11 -0300

    feat: Middleware verifyToken

--- src/middlewares/globals.middleware.ts
+++ src/middlewares/globals.middleware.ts
@@ -6,5 +6,21 @@ export const validateBody =
   (req: Request, res: Response, next: NextFunction): void => {
     req.body = schema.parse(req.body);
 
-    return next();
-  };
+
+export const verifyToken = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+): void => {
+  const { authorization } = req.headers;
+
+  if (!authorization)
+    throw new AppError('Missing bearer token', 401);
+
+  const token: string = authorization.split(' ')[1];
+  const decoded = verify(token, process.env.SECRET_KEY!);
+  res.locals = { ...res.locals, decoded };
+
+  return next();
+};
+
