21b5a9819da7c933d9423704383096e70e3cffc7
fabiodelllima <f.f.fabiodelima@gmail.com>
2023-11-01 15:12:02 -0300

    feat: Controller de Read para toda a lista de Users

--- src/controllers/users.controller.ts
+++ src/controllers/users.controller.ts
@@ -1,5 +1,8 @@
 import { Request, Response } from 'express';
-import { createUserService } from '../services/users.service';
+import {
+  createUserService,
+  readAllUsersService,
+} from '../services/users.service';
 import { TUserReturn } from '../interfaces/users.interface';
 
 export const createUserController = async (
@@ -9,4 +12,13 @@ export const createUserController = async (
   const user: TUserReturn = await createUserService(req.body);
 
   return res.status(201).json(user);
+};
+
+export const readAllUsersController = async (
+  req: Request,
+  res: Response
+): Promise<Response> => {
+  const allUsers = await readAllUsersService();
+
+  return res.status(200).json(allUsers);
 };
