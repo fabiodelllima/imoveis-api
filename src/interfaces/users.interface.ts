869c648f343b441feb1929b58852c5815fce9cc1
fabiodelllima <f.f.fabiodelima@gmail.com>
2023-11-01 15:09:33 -0300

    refactor: Tipagem de User para retorno de GET de toda a lista

--- src/interfaces/users.interface.ts
+++ src/interfaces/users.interface.ts
@@ -10,5 +10,6 @@ export type TUserCreate = z.infer<typeof createUserSchema>;
 export type TUserBodyUpdate = Omit<TUserCreate, 'admin'>;
 export type TUserUpdate = DeepPartial<TUserBodyUpdate>;
 export type TUserReturn = z.infer<typeof userReturnSchema>;
+export type TUserReadReturn = TUserReturn[];
 export type TUserLogin = z.infer<typeof userLoginSchema>;
 export type TUserLoginReturn = { token: string };
