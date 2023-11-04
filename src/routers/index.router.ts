47b5e1000d21c673fbc3928d4b56b3cface10c24
fabiodelllima <f.f.fabiodelima@gmail.com>
2023-11-04 05:16:15 -0300

    feat: Rota de RealEstates

--- src/routers/index.router.ts
+++ src/routers/index.router.ts
@@ -2,9 +2,11 @@ import { Router } from 'express';
 import { userRouter } from './users.router';
 import { loginRouter } from './session.router';
 import { categoryRouter } from './categories.router';
+import { realEstateRouter } from './realEstates.router';
 
 export const routes: Router = Router();
 
 routes.use('/users', userRouter);
 routes.use('/login', loginRouter);
 routes.use('/categories', categoryRouter);
+routes.use('/realEstate', realEstateRouter);
