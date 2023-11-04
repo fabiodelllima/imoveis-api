e7863ca0673a1a89f57b2f57c260e4730f7e4665
fabiodelllima <f.f.fabiodelima@gmail.com>
2023-11-04 04:17:57 -0300

    refactor: Rota de POST para Categories

--- src/routers/categories.router.ts
+++ src/routers/categories.router.ts
@@ -1,19 +1,14 @@
 import { Router } from 'express';
 import { createCategoryController } from '../controllers/categories.controller';
 import { createCategorySchema } from '../schemas/categories.schema';
-import {
-  validateBody,
-  verifyAdmin,
-  verifyToken,
-} from '../middlewares/globals.middleware';
+import { validateBody } from '../middlewares/globals.middleware';
 
 export const categoryRouter: Router = Router();
 
 categoryRouter.post(
   '/',
   validateBody(createCategorySchema),
-  verifyToken,
-  verifyAdmin,
   createCategoryController
 );
 
+categoryRouter.get('/');
