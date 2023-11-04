bd8a58c979bad24540bd9b7b355cab78a26a03db
fabiodelllima <f.f.fabiodelima@gmail.com>
2023-11-04 04:12:25 -0300

    feat: Rota de Category

--- src/routers/index.router.ts
+++ src/routers/index.router.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express';
 import { userRouter } from './users.router';
 import { loginRouter } from './session.router';
+import { categoryRouter } from './categories.router';
 
 export const routes: Router = Router();
 
 routes.use('/users', userRouter);
 routes.use('/login', loginRouter);
+routes.use('/categories', categoryRouter);
