223f140489beb88f15687174d42d19dd46a819e9
fabiodelllima <f.f.fabiodelima@gmail.com>
2023-11-04 05:42:12 -0300

    feat: Rota de Schedules

--- src/routers/index.router.ts
+++ src/routers/index.router.ts
@@ -3,6 +3,7 @@ import { userRouter } from './users.router';
 import { loginRouter } from './session.router';
 import { categoryRouter } from './categories.router';
 import { realEstateRouter } from './realEstates.router';
+import { scheduleRouter } from './schedules.router';
 
 export const routes: Router = Router();
 
@@ -10,3 +11,4 @@ routes.use('/users', userRouter);
 routes.use('/login', loginRouter);
 routes.use('/categories', categoryRouter);
 routes.use('/realEstate', realEstateRouter);
+routes.use('/schedules', scheduleRouter);
