75595ce04b416954708228fa7d05ea28743af255
fabiodelllima <f.f.fabiodelima@gmail.com>
2023-11-07 12:37:16 -0300

    feat: Service de Read em RealEstate By Category

--- src/services/categories.service.ts
+++ src/services/categories.service.ts
@@ -2,6 +2,7 @@ import { TReadAllCategories } from './../interfaces/categories.interface';
 import Category from '../entities/Category.entity';
 import { TCreateCategory } from '../interfaces/categories.interface';
 import { categoryRepo } from '../repositories';
+import AppError from '../errors/AppError.error';
 
 export const createCategoryService = async (
   data: TCreateCategory
@@ -13,3 +14,20 @@ export const readAllCategoriesService =
   async (): Promise<TReadAllCategories> => {
     return await categoryRepo.find();
   };
+
+export const readRealEstateByCategoryService = async (
+  id: number
+): Promise<Category> => {
+  const category: Category | null = await categoryRepo.findOne({
+    where: {
+      id,
+    },
+    relations: {
+      realEstate: true,
+    },
+  });
+
+  if (!category) throw new AppError('Category not found', 404);
+
+  return category;
+};
