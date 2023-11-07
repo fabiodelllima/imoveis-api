0443f47cfcde1a40bf054fd13a424827610e21c6
fabiodelllima <f.f.fabiodelima@gmail.com>
2023-11-07 01:00:47 -0300

    refactor: Recriação de banco

---
+++ src/migrations/1699328899085-InitialMigration.ts
@@ -0,0 +1,18 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class InitialMigration1699328899085 implements MigrationInterface {
+    name = 'InitialMigration1699328899085'
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE "realEstates" ALTER COLUMN "sold" SET DEFAULT '0'`);
+        await queryRunner.query(`ALTER TABLE "realEstates" ALTER COLUMN "createdtAt" SET DEFAULT now()`);
+        await queryRunner.query(`ALTER TABLE "realEstates" ALTER COLUMN "updatedAt" SET DEFAULT now()`);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE "realEstates" ALTER COLUMN "updatedAt" DROP DEFAULT`);
+        await queryRunner.query(`ALTER TABLE "realEstates" ALTER COLUMN "createdtAt" DROP DEFAULT`);
+        await queryRunner.query(`ALTER TABLE "realEstates" ALTER COLUMN "sold" SET DEFAULT false`);
+    }
+
+}
