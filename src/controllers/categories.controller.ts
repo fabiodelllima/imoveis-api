ad7d9d324a8e70d5b12f6bf88937177a5d5326b9
fabiodelllima <f.f.fabiodelima@gmail.com>
2023-11-07 12:37:54 -0300

    feat: Controller de Read em RealEstate By Category

--- src/controllers/categories.controller.ts
+++ src/controllers/categories.controller.ts
@@ -2,6 +2,7 @@ import { Request, Response } from 'express';
 import {
   createCategoryService,
   readAllCategoriesService,
+  readRealEstateByCategoryService,
 } from '../services/categories.service';
 
 export const createCategoryController = async (
@@ -20,4 +21,17 @@ export const readAllCategoriesController = async (
   const allCategories = await readAllCategoriesService();
 
   return res.status(200).json(allCategories);
+};
+
+export const readRealEstateByCategoryController = async (
+  req: Request,
+  res: Response
+): Promise<Response> => {
+  const { id } = req.params;
+
+  const realEstate = await readRealEstateByCategoryService(
+    Number(id)
+  );
+
+  return res.status(200).json(realEstate);
 };
